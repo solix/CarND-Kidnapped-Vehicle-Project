de3e5c9d2d21ac1047a04dc422f8e1463c5f8dda
solix <soheil.jahanshahi@gmail.com>
2017-07-21 20:14:38 +0200

    update weight implemented, debugging

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -30,9 +30,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 
 	// Set standard deviations for x, y, and psi.
 	 num_particles = 100;
-	 std_x = 2;
-	 std_y = 2;
-	 std_psi = 0.05;
+	
 
 	 	
 	// normal distributions for x, y and psi
@@ -46,7 +44,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 		
 		//Sample  and from these normal distrubtions like this: 
 		Particle p;
-		p.id =1;
+		p.id =i;
 		p.x = dist_x(gen);
 		p.y = dist_y(gen);
 		p.theta = dist_psi(gen);
@@ -127,6 +125,45 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 	// NOTE: this method will NOT be called by the grading code. But you will probably find it useful to 
 	//   implement this method and use it as a helper during the updateWeights phase.
 
+
+		LandmarkObs landmark_obj;
+
+		for(int i = 0; i < observations.size(); i++){
+			landmark_obj = observations[i];
+
+			//minimum distance		
+			double min_dis = std::numeric_limits<double>::max();
+
+			//calculate distance for every landmark
+			for(int j = 0 ; j < predicted.size(); j++  ){
+
+
+				//create variables to hold current input values 
+				double current_dist;
+				double meas_x = landmark_obj.x;
+				double meas_y = landmark_obj.y;
+				double mu_x = predicted[j].x;
+				double mu_y = predicted[j].y;
+				int id = predicted[j].id;
+				current_dist = dist(meas_x,meas_y,mu_x,mu_y);
+
+				if(current_dist < min_dis){
+					min_dis = current_dist;
+					landmark_obj.id = id;
+
+				}
+				landmark_obj.id = id;
+
+
+
+			}
+
+
+
+
+
+		}
+
 }
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
@@ -142,13 +179,102 @@ void ParticleFilter::updateWeights(double sensor_range, double std_landmark[],
 	//   3.33
 	//   http://planning.cs.uiuc.edu/node99.html
 
-	// for(int p = 0;i < num_particles; i++){
-	// 	for(int o = 0; o< observations.length; i++){
-	// 		if(associations=0){
-	// 			// double meas_x = trans_obs
-	// 		}
-	// 	}
-	// }
+	for(int p = 0; p < num_particles; p++){
+
+		vector<int> associations;
+		vector<double> sense_x;
+		std::vector<double> sense_y;
+		double p_x = particles[p].x;
+		double p_y = particles[p].y;
+		double p_theta = particles[p].theta;
+		int p_id = particles[p].id;
+		double p_weight = particles[p].weight;
+
+		//filter landmarks
+		//ditch the too far landmarks
+
+		vector<LandmarkObs> predicted;
+
+		for(int j =0; j < map_landmarks.landmark_list.size(); j++)
+		{
+			
+			double mu_x = map_landmarks.landmark_list[j].x_f;
+			double mu_y = map_landmarks.landmark_list[j].y_f;
+			int id = map_landmarks.landmark_list[j].id_i;
+
+			if(fabs(p_x - mu_x) <= sensor_range  && fabs(p_y - mu_y) <= sensor_range){
+				LandmarkObs landmark;
+				landmark.x = mu_x;
+				landmark.y = mu_y;
+				landmark.id = id;
+				predicted.push_back(landmark);
+
+			}
+
+	   }
+
+		//transform  
+		std::vector<LandmarkObs> obs_transform2d;
+		LandmarkObs landmark_obj;
+
+		for(int i=0;i<observations.size();i++){
+			LandmarkObs landmark_obj_trans;
+			landmark_obj = observations[i];
+
+			landmark_obj_trans.x = particles[p].x+(landmark_obj.x*cos(particles[p].theta)-landmark_obj.y*sin(particles[p].theta));
+			landmark_obj_trans.y = particles[p].y+(landmark_obj.x*sin(particles[p].theta)+landmark_obj.y*cos(particles[p].theta));
+
+			obs_transform2d.push_back(landmark_obj_trans);
+
+		}
+
+
+		dataAssociation(predicted,obs_transform2d);
+
+		particles[p].weight = 1.0;
+		double x,y,x_mu,y_mu;
+		int landmark_id , map_id;
+
+		for(int t = 0; t < obs_transform2d.size(); t++){
+
+			x = obs_transform2d[t].x;
+			y = obs_transform2d[t].y;
+			landmark_id = obs_transform2d[t].id;
+
+			for(int j = 0 ; j < predicted.size(); j++  ){
+				map_id = predicted[j].id;
+				if(landmark_id == map_id){
+					x_mu = predicted[j].x;
+					y_mu = predicted[j].y;
+				}
+		}
+	
+		double x_xmu_pow = pow(x-x_mu,2); 
+		double y_ymu_pow = pow(y-y_mu,2);
+		double std_x = std_landmark[0];
+		double std_y = std_landmark[1];
+		double std_x_pow = pow(std_x,2);
+		double std_y_pow = pow(std_y,2);
+		double normalizer = 1/(2*M_PI*std_x*std_y);
+		double e_num = exp(-1*((x_xmu_pow/(2*std_x_pow))+(y_ymu_pow/(2*std_y_pow)))); 
+
+		//weight for particular measurement
+		double gauss_pr = normalizer*e_num;
+
+		if(gauss_pr>0)
+		{
+		p_weight *= gauss_pr;
+		}
+		sense_x.push_back(x);
+		sense_y.push_back(y);
+		associations.push_back(t+1);
+	}
+
+
+		particles[p]= SetAssociations(particles[p],associations,sense_x,sense_y);
+
+	
+	}
 
 }
 
